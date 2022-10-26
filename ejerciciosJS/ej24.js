ea353cc7e35753ab76add91d72bd7f98c3ef389c
Mohamed KB <mohabel98@gmail,com>
2022-10-26 15:49:01 +0200

    ej24 hecho

---
+++ ejerciciosJS/ej24.js
@@ -0,0 +1,10 @@
+function numAleatorio(min , max) {
+
+    return console.log(Math.floor(Math.random()*(max-min+1)+min));
+}
+
+numAleatorio(1,10);
+numAleatorio(1,10);
+numAleatorio(1,10);
+numAleatorio(1,10);
+numAleatorio(1,10);
