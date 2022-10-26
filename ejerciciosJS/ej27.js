3ccaab4cc8ca0058e3be3f2f006a990cc615a64c
Mohamed KB <mohabel98@gmail,com>
2022-10-26 16:01:15 +0200

    ej27 hecho

---
+++ ejerciciosJS/ej27.js
@@ -0,0 +1,11 @@
+function esPar(num) {
+    if ((num%2) == 0) {
+        return console.log(`El numero ${num} es par.`);
+    } else {
+        return console.log(`El numero ${num} es impar.`);
+    } 
+}
+
+esPar(4);
+esPar(7);
+esPar(18);
